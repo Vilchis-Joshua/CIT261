9abb8cea002b78629d05bcbccf6d7db2f8ab5396
jsvilchis <vil15009@byui.edu>
2018-01-17 23:07:06 -0700

    Fixing up the guessFavorite function

--- week01/week01.js
+++ week01/week01.js
@@ -1,3 +1,9 @@
 function confirmSport(guess) {
-    console.log(guess);
+    if (guess.toLower == soccer) {
+        alert("YOU WIN!");
+    } else if (guess.toLower == tennis) {
+        alert("Close! That's my second favorite. But...what is \"close\"?");
+    } else {
+        alert("No");
+    }
 }
