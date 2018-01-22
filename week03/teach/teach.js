5730bb396aaef1e8d00f16368a4689658105e625
jsvilchis <vil15009@byui.edu>
2018-01-22 02:39:53 -0700

    Added functionality

--- week03/teach/teach.js
+++ week03/teach/teach.js
@@ -1,14 +1,12 @@
 function createFirstObject() {
-    if (document.getElementById("firstpart").innerHTML == null) {
-        var first_person = {
-            firstName: "Joseph",
-            lastName: "Smith",
-            gender: "male",
+    var first_person = {
+        firstName: "Joseph",
+        lastName: "Smith",
+        gender: "male",
 
-            fullName: function () {
-                return this.firstName + " " + this.lastName;
-            }
+        fullName: function () {
+            return this.firstName + " " + this.lastName;
         }
-        document.getElementById("firstpart").innerHTML += first_person.fullName();
     }
+    document.getElementById("firstpart").innerHTML = first_person.fullName();
 }
