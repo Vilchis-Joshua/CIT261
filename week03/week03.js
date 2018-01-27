5c1f266a073c64359fa478ae9eb3838f2193a591
jsvilchis <vil15009@byui.edu>
2018-01-27 16:52:05 -0700

    gettting it going

--- week03/week03.js
+++ week03/week03.js
@@ -4,5 +4,11 @@ function generateString() {
 
 
 function toParse() {
+    var text = '{ "employees" : [' +
+        '{ "firstName":"Josh" , "lastName":"Vilchis", "gender":"male", "age":"22" },' +
+        '{ "firstName":"Anna" , "lastName":"Smith", "gender":"female" }, "age":"22"' +
+        '{ "firstName":"Peter" , "lastName":"Jones", "gender":"male", "age":"1" } ]}';
 
+    var obj = JSON.parse(text);
+    document.getElementById("displayObject").innerHTML = obj;
 }
