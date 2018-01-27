9d47ddf56d42db4a03fe0f1c571316ec080dc0f8
jsvilchis <vil15009@byui.edu>
2018-01-27 16:57:57 -0700

    testing

--- week03/week03.js
+++ week03/week03.js
@@ -4,10 +4,14 @@ function generateString() {
 
 
 function toParse() {
+    //var text = '{ "employees" : [' +
+    //    '{ "firstName":"Josh" , "lastName":"Vilchis", "gender":"male", "age":22 }' +
+    //    '{ "firstName":"Anna" , "lastName":"Smith", "gender":"female", "age":22 }' +
+    //    '{ "firstName":"Peter" , "lastName":"Jones", "gender":"male", "age":1 } ]}';
     var text = '{ "employees" : [' +
-        '{ "firstName":"Josh" , "lastName":"Vilchis", "gender":"male", "age":22 }' +
-        '{ "firstName":"Anna" , "lastName":"Smith", "gender":"female", "age":22 }' +
-        '{ "firstName":"Peter" , "lastName":"Jones", "gender":"male", "age":1 } ]}';
+        '{ "firstName":"John" , "lastName":"Doe" },' +
+        '{ "firstName":"Anna" , "lastName":"Smith" },' +
+        '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
 
     var obj = JSON.parse(text);
     document.getElementById("displayObject").innerHTML = obj;
