efca39e69a90f279beb02f59f4deae0483293d51
jsvilchis <vil15009@byui.edu>
2018-01-27 16:54:00 -0700

    fixed typo in week03.js

--- week03/week03.js
+++ week03/week03.js
@@ -6,7 +6,7 @@ function generateString() {
 function toParse() {
     var text = '{ "employees" : [' +
         '{ "firstName":"Josh" , "lastName":"Vilchis", "gender":"male", "age":"22" },' +
-        '{ "firstName":"Anna" , "lastName":"Smith", "gender":"female" }, "age":"22"' +
+        '{ "firstName":"Anna" , "lastName":"Smith", "gender":"female", "age":"22" }' +
         '{ "firstName":"Peter" , "lastName":"Jones", "gender":"male", "age":"1" } ]}';
 
     var obj = JSON.parse(text);
