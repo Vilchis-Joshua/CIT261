2aab92ed311a94470339526a165a8ad9b07134b3
jsvilchis <vil15009@byui.edu>
2018-01-27 17:11:10 -0700

    done

--- week03/week03.js
+++ week03/week03.js
@@ -1,5 +1,3 @@
-var obj;
-
 function generateString() {
     document.getElementsByClassName("json")[0].style.visibility = 'visible';
 }
@@ -11,11 +9,18 @@ function toParse() {
         '{ "firstName":"Anna" , "lastName":"Smith", "gender":"female", "age":66 },' +
         '{ "firstName":"Peter" , "lastName":"Jones", "gender":"male", "age":1 } ]}';
 
-    obj = JSON.parse(text);
+    var obj = JSON.parse(text);
     document.getElementById("displayObject").innerHTML = obj;
 }
 
 function generateName() {
+    var text = '{ "employees" : [' +
+        '{ "firstName":"Josh" , "lastName":"Vilchis", "gender":"male", "age":22 },' +
+        '{ "firstName":"Anna" , "lastName":"Smith", "gender":"female", "age":66 },' +
+        '{ "firstName":"Peter" , "lastName":"Jones", "gender":"male", "age":1 } ]}';
+
+    var obj = JSON.parse(text);
+    document.getElementById("displayObject").innerHTML = obj;
     var num = Math.floor(Math.random() * 3);
     document.getElementById("displayName").innerHTML =  obj.employees[num].firstName + " " + obj.employees[num].lastName + " is a "
         obj.employees[num].gender + " and is " + obj.employees[1].age + " years old." 
