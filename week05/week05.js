a0f85bbc21f33b2c7d03e94fbca0dfd7090432dc
jsvilchis <vil15009@byui.edu>
2018-01-31 21:41:07 -0700

    still trying to debug

--- week05/week05.js
+++ week05/week05.js
@@ -1,5 +1,7 @@
 function localPractice() {
     var x = document.getElementsByName("exampleInput").value;
+    var y = document.getElementById("exampleInput").value;
     console.log(x);
+    console.log(y);
     localStorage.setItem("example:", x);
 }
