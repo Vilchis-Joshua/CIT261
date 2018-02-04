d021c1a8b17708dbf39f874d6ae28281d45f083e
jsvilchis <vil15009@byui.edu>
2018-02-03 21:42:42 -0700

    added jsFiles

--- Project/jsFiles/project.js
+++ Project/jsFiles/project.js
@@ -1,4 +1,4 @@
-var s;
+var snake;
 
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
@@ -19,9 +19,22 @@ function setup() {
 
 function draw() {
     background(51);
-    s.update();
-    s.show();
+    snake.update();
+    snake.show();
 }
+
+function keyPressed() {
+    if (keyCode === UP_ARROW) {
+        snake.direction(0, -1);
+    } else if (keyCode === DOWN_ARROW) {
+        snake.direction(0, 1);
+    } else if (keyCode === RIGHT_ARROW) {
+        snake.direction(-1, 0);
+    } else if (keyCode === DOWN_ARROW) {
+        snake.direction(1, 0);
+    }
+}
+
 
 function Snake() {
     this.x = 0;
@@ -29,6 +42,7 @@ function Snake() {
     this.xspeed = 1;
     this.yspeed = 0;
 
+    
     this.update = function () {
         this.x = this.x + this.xspeed;
         this.y = this.y + this.yspeed;
@@ -37,5 +51,9 @@ function Snake() {
     this.show = function () {
         fill(255);
         rect(this.x, this.y, 10, 10);
+    }
+
+    this.direction(x, y) {
+    
     }
 }
