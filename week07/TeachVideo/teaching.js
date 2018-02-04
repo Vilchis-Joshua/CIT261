f69ad97c8b7e4ac1cbe21fe908a1fe2e7c8ae706
jsvilchis <vil15009@byui.edu>
2018-02-03 17:00:49 -0700

    testing

---
+++ week07/TeachVideo/teaching.js
@@ -0,0 +1,27 @@
+function makeVisible() {
+    document.getElementById("box").style.visibility = 'visible';
+}
+
+function makeHidden() {
+    document.getElementById("box").style.visibility = 'hidden';
+
+}
+
+function changeColor() {
+    document.getElementById("box").style.visibility = 'visible';
+
+}
+
+function makeVisible() {
+    document.getElementById("box").style.visibility = 'visible';
+
+}
+
+function leftToRight() {
+    document.getElementById("box").style.visibility = 'visible';
+
+}
+
+function rightToLeft() {
+    document.getElementById("box").style.visibility = 'visible';
+}
