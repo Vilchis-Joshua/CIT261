f20c2bfdac71384e0ff7d1189c7cd76f69fa4b23
jsvilchis <vil15009@byui.edu>
2018-02-05 23:04:12 -0700

    l

--- Project/jsFiles/project.js
+++ Project/jsFiles/project.js
@@ -1,9 +1,3 @@
-var snake;
-var food;
-var scl = 20;
-var dir;
-var f;
-
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
@@ -15,48 +9,7 @@ function loadDoc() {
     xhttp.send();
 }
 
-
-
-
-//Game play================================================================================
-
-function setup() {
-    createCanvas(600, 600);
-    frameRate(10);
-
-    snake = new Snake();
-    food = new Food();
-    food.pickLocation();
+function turnIntoString() {
+    var obj = JSON.stringify(snake);
+    console.log(obj);
 }
-
-function draw() {
-    background(51);
-
-    snake.die();
-    snake.update();
-    snake.show();
-
-    food.show();
-
-    if (snake.eat(food.pos)) {
-        food.pickLocation();
-    } 
-}
-
-
-function keyPressed() {
-    if (keyCode === UP_ARROW) {
-        snake.direction(0, -1);
-    } else if (keyCode === DOWN_ARROW) {
-        snake.direction(0, 1);
-    } else if (keyCode === RIGHT_ARROW) {
-        snake.direction(1, 0);
-    } else if (keyCode === LEFT_ARROW) {
-        snake.direction(-1, 0);
-    }
-}
-
-
-
-
-
