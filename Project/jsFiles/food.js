9803593028b0380fe37d6cd3a4fc8464b43e595b
jsvilchis <vil15009@byui.edu>
2018-02-05 21:54:24 -0700

    done?

--- Project/jsFiles/food.js
+++ Project/jsFiles/food.js
@@ -3,20 +3,12 @@ function Food() {
     this.y = 0;
     this.pos = createVector(floor(random(width / scl), floor(random(height / scl))));
 
-    this.update = function () {
-
-    }
-
     this.show = function () {
         fill(255, 0, 100);
         rect(this.pos.x, this.pos.y, scl, scl);
     }
 
-    this.initialPos() {
-    
-    }
-
-    this.pickLocation() {
+    this.pickLocation = function() {
         var cols = floor(width / scl);
         var rows = floor(height / scl);
         this.pos = createVector(floor(random(cols), floor(random(rows))));
