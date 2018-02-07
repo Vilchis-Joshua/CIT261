3786d00bb4b1d0b97b23a8a65b8530c972b99904
jsvilchis <vil15009@byui.edu>
2018-02-07 15:32:02 -0700

    got the other two functions working

--- week06/week06.js
+++ week06/week06.js
@@ -12,11 +12,29 @@ function createButton() {
 }
 
 function createDiv() {
+    //Create div elements
+    var btn = document.createElement("DIV");
 
+    //create text
+    var t = document.getElementById("name").value;
+    var text = document.createTextNode(t);
+
+    btn.appendChild(text);
+
+    document.body.appendChild(btn);
 }
 
 function createH2() {
+    //Create h2 elements
+    var btn = document.createElement("H2");
 
+    //create text
+    var t = document.getElementById("name").value;
+    var text = document.createTextNode(t);
+
+    btn.appendChild(text);
+
+    document.body.appendChild(btn);
 }
 
 
