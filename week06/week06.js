4d91bfb8fdb8e0ec79b9fa3f17fb07cadccd7acd
jsvilchis <vil15009@byui.edu>
2018-02-07 15:13:18 -0700

    working?

---
+++ week06/week06.js
@@ -0,0 +1,27 @@
+function createButton() {
+    //Create button elements
+    var button = document.createElement("button");
+
+    //create text
+}
+
+function createDiv() {
+
+}
+
+function createH2() {
+
+}
+
+
+function deleteButton() {
+
+}
+
+function deleteDiv() {
+
+}
+
+function deleteH2() {
+
+}
