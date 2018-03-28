e7f07b02aab7834cf60cb13dfaf0207120f1025f
jsvilchis <vil15009@byui.edu>
2018-03-27 21:39:36 -0400

    why not working?

--- FinalProject/main.js
+++ FinalProject/main.js
@@ -68,9 +68,9 @@ function chooseVerse() {
     if (obj.sections) {
         for (i in obj.sections) {
             console.log('1: ' + obj.sections[i].section);
-            if (obj.sections[i].section === v) {
-                console.log('2: ' + obj.sections[i].section.verses[j]);
+            if (obj.sections[i].section == v) {
                 for (j in obj.sections[i].section.verses[j]) {
+                    console.log('2: ' + obj.sections[i].section.verses[j]);
                     console.log('3: ' + obj.sections[i].section.reference[j]);
                     var opt = document.createElement('option');
                     opt.setAttribute('id', obj.sections[i].section.reference[j]);
